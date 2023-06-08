14486c541b5879774f0be595e4f5442633d6cdc1
Anaëlle Ighil-Cardoso <anaelle.ighil@gmail.com>
2023-06-08 18:05:09 +0200

    function to generate data

--- seed.ts
+++ seed.ts
@@ -4,19 +4,62 @@ import {faker} from '@faker-js/faker/locale/fr';
 enum Gender { Masc ='M', Fem ='F'};
 enum Number { Single='S', Plural ='P'};
 
-const randomSubject = {
-    value : faker.word.noun(),
-    gender: faker.helpers.enumValue(Gender),
-    number : faker.helpers.enumValue(Number)
+interface Subject {
+    value: string;
+    gender: string;
+    number: string;
 }
-console.log(randomSubject);
 
-const randomVerb = {
+interface Verb {
+value :string;
+}
+
+interface Complement {
+    value:string;
+}
+
+const generationDataValue : number= 69;
+
+function generateSubject(generationDataValue:number):any{
+
+    const Subjects :Array<Subject>=[]
+    for (let i=0; i<generationDataValue; i++){
+        const randomSubject: Subject = {
+            value : faker.word.noun(),
+            gender: faker.helpers.enumValue(Gender),
+            number : faker.helpers.enumValue(Number)
+        }
+        console.log(randomSubject)
+        Subjects.push(randomSubject);
+    }
+    return Subjects;
+}
+
+
+function generateVerb(generationDataValue:number):any{
+    const Verbs: Array<Verb>=[];
+    for (let i=0; i<generationDataValue; i++){
+          const randomVerb: Verb = {
     value : faker.word.verb()
 }
-console.log(randomVerb)
 
-const randomComplent = {
+Verbs.push(randomVerb);
+    }
+    return Verbs
+}
+
+function generateComplement( generationDataValue: number):any {
+    const Complements: Array<Complement>= [];
+    for (let i=0; i<generationDataValue; i++){
+        const randomComplent :Verb = {
     value : faker.word.adjective()
 }
-console.log(randomComplent)
+
+Complements.push(randomComplent);
+    }
+    return Complements
+}
+
+generateSubject(generationDataValue)
+
+
